4e46c2c915f407e2a72c0982682fce6b38978af0
aleksey <fearerror@yandex.ru>
2016-12-03 18:41:26 +0400

    add test

--- chapter_001/src/main/java/ru/avoronin/Calculate.java
+++ chapter_001/src/main/java/ru/avoronin/Calculate.java
@@ -3,11 +3,6 @@ package avoronin;
 public class Calculate {
 
    public static void main(String[] args) {
-      Calculate calc = new Calculate();
-      System.out.println(calc.showText("World"));
-   }
-
-   public String showText(String text) {
-      return String.format("%s %s %s", text, text, text);
+      	System.out.println("Hello World");
    }
 }
--- chapter_001/src/test/java/ru/avoronin/CalculateTest.java
+++ chapter_001/src/test/java/ru/avoronin/CalculateTest.java
@@ -3,20 +3,16 @@ package avoronin;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class CalculateTest {
     
     @Test
     public void whenSetWorldInShowTextThenReturnInThreeWorld() {
-        Calculate calc = new Calculate();
-        String actualText = calc.showText("World");
-        assertThat(actualText, is("World World World"));
-    }
-
-    @Test
-    public void whenSetNullShowTextThenReturnTwoSpaces() {
-        Calculate calc = new Calculate();
-        String actualText = calc.showText("");
-        assertThat(actualText, is("  "));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
     }
 }
