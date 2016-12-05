a865218f9dc47fb90e4ada12ac7e81f72b2236d2
aleksey <fearerror@yandex.ru>
2016-12-05 23:49:30 +0400

    Max class and MaxText class created

---
+++ chapter1/src/main/java/ru/avoronin/Max.java
@@ -0,0 +1,28 @@
+package ru.avoronin;
+
+/**
+ * Клас для нахождения максимального числа.
+ */
+public class Max {
+
+    /**
+     * Метод получает на вход 2 числа и возвращает то которое больше.
+     * @param first первое число.
+     * @param second второе число.
+     * @return возвращает большее число.
+     */
+    public int findMaxTwoNumber(final int first, final int second) {
+        return first > second ? first : second;
+    }
+
+    /**
+     * Находит большее из трех чисел.
+     * @param first первое число.
+     * @param second второе число.
+     * @param third третье число.
+     * @return возвращает большее из трех чисел.
+     */
+    public int findLargeNumberOfThree(final int first, final int second, final int third) {
+        return findMaxTwoNumber(findMaxTwoNumber(first, second), third);
+    }
+}
---
+++ chapter1/src/test/java/ru/avoronin/MaxTest.java
@@ -0,0 +1,93 @@
+package ru.avoronin;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест класса Max.
+ */
+public class MaxTest {
+
+    /**
+     * Тест возвращает первое число, если оно больше второго.
+     */
+    @Test
+    public void whenTheFirstMoreThanTheSecondTheFirstReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 3;
+        assertThat(first, is(max.findMaxTwoNumber(first, second)));
+    }
+
+    /**
+     * Тест возвращает второе число, если оно больше первого.
+     */
+    @Test
+    public void whenTheSecondMoreThanTheFirstTheSecondReturn() {
+        Max max = new Max();
+        final int first = 7;
+        final int second = 9;
+        assertThat(second, is(max.findMaxTwoNumber(first, second)));
+    }
+
+    /**
+     * Тест возвращает первое число, если оба числа равны.
+     */
+    @Test
+    public void whenTheFirstEqualsThanTheSecondTheFirstReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 5;
+        assertThat(first, is(max.findMaxTwoNumber(first, second)));
+    }
+
+
+    /**
+     * Тест возвращает первое число, если оно больше второго и третьего.
+     */
+    @Test
+    public void whenTheFirstMoreThanTheSecondAndThirdTheFirstReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 3;
+        final int third = 4;
+        assertThat(first, is(max.findLargeNumberOfThree(first, second, third)));
+    }
+
+    /**
+     * Тест возвращает второе число, если оно больше первого и третьего.
+     */
+    @Test
+    public void whenTheSecondMoreThanTheFirstAndThirdTheSecondReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 7;
+        final int third = 4;
+        assertThat(second, is(max.findLargeNumberOfThree(first, second, third)));
+    }
+
+    /**
+     * Тест возвращает второе число, если оно больше первого и третьего.
+     */
+    @Test
+    public void whenTheThirdMoreThanTheFirstAndSecondTheThirdReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 7;
+        final int third = 11;
+        assertThat(third, is(max.findLargeNumberOfThree(first, second, third)));
+    }
+
+    /**
+     * Тест возвращает второе число, если оно больше первого и третьего.
+     */
+    @Test
+    public void whenTheFirstAndSecondAndThirdEqualsThenFirstReturn() {
+        Max max = new Max();
+        final int first = 5;
+        final int second = 5;
+        final int third = 5;
+        assertThat(first, is(max.findLargeNumberOfThree(first, second, third)));
+    }
+}
