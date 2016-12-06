aa04ca5b2dfc14a5ac185e4f408af33941637a2f
aleksey <fearerror@yandex.ru>
2016-12-06 18:16:17 +0400

    Paint and PaintTest class created

---
+++ chapter1/src/main/java/ru/job4j/paint/Paint.java
@@ -0,0 +1,30 @@
+package ru.job4j.paint;
+
+/**
+ * Класс строит пирамиду из символов.
+ */
+public class Paint {
+
+    /**
+     * Метод рисуте пирамиду высотой h.
+     * @param h высота пирамиды.
+     * @return возвращает пустую строку если параметр h меньше или равен нулю.
+     * в остальных случаях возвращает заполненную символами строку.
+     */
+    public String piramide(int h) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= h; i++) {
+            //ставит отступы для пирамиды с лева.
+            for (int j = 1; j <= h - i; j++) {
+                sb.append(" ");
+            }
+
+            //ставит символы которыми рисуется пирамида.
+            for (int k = 1; k <= i; k++) {
+                sb.append("^ ");
+            }
+            sb.append("\r\n");
+        }
+        return h <= 0 ? "" : sb.toString();
+    }
+}
---
+++ chapter1/src/test/java/ru/job4j/paint_test/PaintTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.paint_test;
+
+import ru.job4j.paint.Paint;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест для класса Paint.
+ */
+public class PaintTest {
+
+    /**
+     * Тест рисование пирамиды.
+     * Если в тест передали значение h равным 0.
+     */
+    @Test
+    public void whenHeightEqualsZeroThenReturnEmptyString() {
+        final Paint p = new Paint();
+        final int height = 0;
+        String result = "";
+        assertThat(result, is(p.piramide(height)));
+    }
+
+    /**
+     * Тест рисование пирамиды.
+     * Если в тест передали значение h равным 1.
+     */
+    @Test
+    public void whenHeightEqualsOneThenReturnOneSymbolString() {
+        final Paint p = new Paint();
+        final int height = 1;
+        String result = "^ \r\n";
+        assertThat(result, is(p.piramide(height)));
+    }
+
+    /**
+     * Тест рисование пирамиды.
+     * Если в тест передали значение h больше 1.
+     */
+    @Test
+    public void whenHeightMoreThatOneThenReturnString() {
+        final Paint p = new Paint();
+        final int height = 2;
+        String result = " ^ \r\n^ ^ \r\n";
+        assertThat(result, is(p.piramide(height)));
+    }
+}
