9097abe7125769478b34aeef043985271c9fc057
aleksey <fearerror@yandex.ru>
2016-12-08 16:26:05 +0400

    MergeArrays and MergeArraysTest class

---
+++ chapter1/src/main/java/ru/job4j/merge_two_arrays/MergeTwoArrays.java
@@ -0,0 +1,7 @@
+package ru.job4j.merge_two_arrays;
+
+/**
+ * Created by Nikromantik on 08.12.2016.
+ */
+public class MergeTwoArrays {
+}
---
+++ chapter1/src/test/java/ru/job4j/merge_two_arrays/MergeTwoArraysTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nikromantik on 08.12.2016.
+ */
+public class MergeTwoArraysTest {
+
+}
