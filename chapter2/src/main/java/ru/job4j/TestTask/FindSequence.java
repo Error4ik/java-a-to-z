35ba10bd940a1871a9ec2ade5036647ae8a2091e
aleksey <fearerror@yandex.ru>
2016-12-21 16:32:12 +0400

    FindSequence and FindSequenceTest class created

---
+++ chapter2/src/main/java/ru/job4j/TestTask/FindSequence.java
@@ -0,0 +1,31 @@
+package ru.job4j.TestTask;
+
+/**
+ * Класс проверят парность скобок в строке.
+ */
+public class FindSequence {
+
+    /**
+     * Возвращает true если скобки последовательность скобок правильная.
+     * @param string строка в которой осуществляется проверка.
+     * @return result.
+     */
+    public boolean find(String string) {
+        boolean result = false;
+        final char[] chars = string.toCharArray();
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                count++;
+            } else if (chars[i] == ')') {
+                count--;
+            }
+        }
+
+        if (count == 0) {
+            result = true;
+
+        }
+        return result;
+    }
+}
---
+++ chapter2/src/test/java/ru/job4j/TestTask/FindSequenceTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.TestTask;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест для класса FindSequence.
+ */
+public class FindSequenceTest {
+
+    /**
+     * Возвращает true если последовательность правильная.
+     */
+    @Test
+    public void whenSequenceTrueThenReturnTrue() {
+        final FindSequence findSequence = new FindSequence();
+        final String sequence = "((((()))))()()()";
+        final boolean result = true;
+        assertThat(findSequence.find(sequence), is(result));
+    }
+
+    /**
+     * Возвращает false если последователность не правильная.
+     */
+    @Test
+    public void whenSequenceFalseThenReturnFalse() {
+        final FindSequence findSequence = new FindSequence();
+        final String sequence = "((((((((((((()))))))(";
+        final boolean result = false;
+        assertThat(findSequence.find(sequence), is(result));
+    }
+
+}
