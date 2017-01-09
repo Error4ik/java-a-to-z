ed465fd790e6bfe296154f8515d1f60ba1f35bab
aleksey <fearerror@yandex.ru>
2017-01-10 01:49:26 +0400

    Palindrome and test class created

---
+++ chapter3/src/main/java/ru/job4j/lesson4/Palindrome.java
@@ -0,0 +1,34 @@
+package ru.job4j.lesson4;
+
+/**
+ * Класс для проверки является ли слово палиндромом.
+ */
+public class Palindrome {
+
+    /**
+     * Метод проверяет является ли слово палиндромом.
+     *
+     * @param word слово которое нужно проверить.
+     * @return возвращает true если является, и false если нет.
+     */
+    public boolean isPalindrome(String word) {
+        boolean result = false;
+
+        if (word.length() % 2 != 0) {
+            char[] chars = word.toLowerCase().toCharArray();
+            int count = 0;
+
+            for (int i = 0; i < chars.length / 2; i++) {
+                if (chars[i] == chars[chars.length - 1 - i]) {
+                    count++;
+                }
+            }
+
+            result = (count == word.length() / 2);
+
+        } else {
+            System.out.print("В слове должно быть не четное количество букв!");
+        }
+        return result;
+    }
+}
---
+++ chapter3/src/test/java/ru/job4j/lesson4/PalindromeTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.lesson4;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Тест класса Palindrome.
+ */
+public class PalindromeTest {
+
+    /**
+     * Тест проверяет что слово является палиндромом.
+     */
+    @Test
+    public void whenIsPalindromeThenReturnTrue() {
+        final Palindrome p = new Palindrome();
+        final String word = "РОтОР";
+        assertThat(p.isPalindrome(word), is(true));
+    }
+
+    /**
+     * Тест проверяет что слово не является палиндромом.
+     */
+    @Test
+    public void whenIsNotPalindromeThenReturnFalse() {
+        final Palindrome p = new Palindrome();
+        final String word = "Короб";
+        final boolean expected = false;
+        assertThat(p.isPalindrome(word), is(expected));
+    }
+
+    /**
+     * Тест проверяет выводится ли сообщение об ошибке если в слове четное количество букв.
+     */
+    @Test
+    public void whenInvalidWordThenPrintMessage() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out, Boolean.parseBoolean(System.getProperty("console.encoding", "utf-8"))));
+        String expected = "В слове должно быть не четное количество букв!";
+        Palindrome palindrome = new Palindrome();
+        palindrome.isPalindrome("privet");
+        assertThat(out.toString(), is(expected));
+    }
+}
