9054045bd03d3ae7e8aa1a0d4434af4337d6ae5d
aleksey <fearerror@yandex.ru>
2017-01-10 03:17:48 +0400

    ref

--- chapter3/src/test/java/ru/job4j/lesson4/PalindromeTest.java
+++ chapter3/src/test/java/ru/job4j/lesson4/PalindromeTest.java
@@ -2,9 +2,6 @@ package ru.job4j.lesson4;
 
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -33,18 +30,5 @@ public class PalindromeTest {
         final String word = "Короб";
         final boolean expected = false;
         assertThat(p.isPalindrome(word), is(expected));
-    }
-
-    /**
-     * Тест проверяет выводится ли сообщение об ошибке если в слове четное количество букв.
-     */
-    @Test
-    public void whenInvalidWordThenPrintMessage() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out, Boolean.parseBoolean(System.getProperty("console.encoding", "utf-8"))));
-        String expected = "В слове должно быть не четное количество букв!";
-        Palindrome palindrome = new Palindrome();
-        palindrome.isPalindrome("privet");
-        assertThat(out.toString(), is(expected));
     }
 }
