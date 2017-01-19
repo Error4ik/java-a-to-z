e7013d9c27e7b44f2724f414e2459c573f08aff2
aleksey <fearerror@yandex.ru>
2017-01-19 19:46:54 +0400

    Client class ref

--- chapter3/src/main/java/ru/job4j/lesson6/client/Client.java
+++ chapter3/src/main/java/ru/job4j/lesson6/client/Client.java
@@ -2,6 +2,9 @@ package ru.job4j.lesson6.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
@@ -9,21 +12,53 @@ import java.net.Socket;
 public class Client {
     private final static int port = 5000;
 
+    private static String name;
+
+
+
     public static void main(String[] args) throws IOException {
         try (Socket socket = new Socket("192.168.1.168", port)) {
 
-            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            System.out.println("Введите фразу для отправки на сервер.");
+            System.out.println("Введите команду для отправки на сервер.");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
             String line;
-            while (true) {
-                line = input.readUTF();
-                System.out.println(line);
-                line = reader.readLine();
+            System.out.println(in.readUTF());
+            while (!("exit".equals(line = reader.readLine()))) {
                 out.writeUTF(line);
+                out.flush();
+                if ("download".equals(line)) {
+                    System.out.println(in.readUTF());
+                    name = reader.readLine();
+                    poluchitFile(in, out);
+                }
+
             }
         }
+    }
+
+    private static void poluchitFile(DataInputStream in, DataOutputStream out) throws IOException {
+        out.writeUTF(name);
+        long length = in.readLong();
+        System.out.println(length);
+        while (length >= 0) {
+            try (FileOutputStream outputStream = new FileOutputStream(new File("./chapter1/" + "/" + name))) {
+                byte[] buffer = new byte[(int) length];
+                int c;
+                while ((c = in.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, c);
+                }
+            }
+            File file = new File("./chapter1/" + "/" + name);
+             if (file.length() == length) {
+                 System.out.println("ok");
+             } else {
+                 System.out.println("not ok");
+             }
+        }
+
     }
 }
