7f49cca7f9dafae66e14b7ad24be7ae7f37b0127
aleksey <fearerror@yandex.ru>
2017-02-11 22:16:17 +0400

    interface IView created.

---
+++ chapter4/srp/src/main/java/ru/job4j/view/IView.java
@@ -0,0 +1,20 @@
+package ru.job4j.view;
+
+/**
+ * Interface output.
+ */
+public interface IView {
+
+    /**
+     * Show menu application.
+     */
+    void showMenu();
+
+    /**
+     * Show result operation.
+     *
+     * @param result result operation.
+     */
+    void showResult(final double result);
+
+}
