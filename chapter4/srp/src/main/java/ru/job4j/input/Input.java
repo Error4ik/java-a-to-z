36ceb60b66c1acb62d4f123c06df5cbe6b020928
aleksey <fearerror@yandex.ru>
2017-02-11 22:15:23 +0400

    interface Input created.

---
+++ chapter4/srp/src/main/java/ru/job4j/input/Input.java
@@ -0,0 +1,14 @@
+package ru.job4j.input;
+
+/**
+ * Input interface.
+ */
+public interface Input {
+
+    /**
+     * The method for obtaining user input.
+     *
+     * @return array input strings.
+     */
+    String[] getInput();
+}
