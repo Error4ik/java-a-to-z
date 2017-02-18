4ad229c4489d81356b16edb202eb852b6973fd55
aleksey <fearerror@yandex.ru>
2017-02-18 23:39:41 +0400

    Interface IControlQuality created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/manager/IControlQuality.java
@@ -0,0 +1,47 @@
+package ru.job4j.manager;
+
+import ru.job4j.food.IFoods;
+import ru.job4j.storage.IStorage;
+import ru.job4j.storage.Storage;
+
+import java.util.List;
+
+/**
+ * Interface Control Quality.
+ */
+public interface IControlQuality {
+
+    /**
+     * Method add storages.
+     *
+     * @param storage storage.
+     */
+    void addStorage(final IStorage storage);
+
+    /**
+     * Method remove storages.
+     *
+     * @param storage storage.
+     */
+    void removeStorage(final IStorage storage);
+
+    /**
+     * Get list storages.
+     *
+     * @return list.
+     */
+    List<IStorage> getStorageList();
+
+    /**
+     * Sorting foods to storages.
+     *
+     * @param foods food.
+     */
+    void sortFoods(final IFoods foods);
+
+    /**
+     * Get number storages.
+     * @return number.
+     */
+    int getAmountOfStorages();
+}
