11689cd1f433ed95c14e257ab32ff7681e17363a
aleksey <fearerror@yandex.ru>
2017-02-18 23:40:42 +0400

    Shop class created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/storage/Shop.java
@@ -0,0 +1,53 @@
+package ru.job4j.storage;
+
+
+import ru.job4j.food.IFoods;
+
+/**
+ *
+ */
+public class Shop extends Storage {
+
+    /**
+     * Condition in add food.
+     */
+    private final double maxLife = 75d;
+
+    /**
+     * Condition in add food.
+     */
+    private final double minLife = 1d;
+
+    /**
+     * Condition in add food.
+     */
+    private final double discountLife = 25d;
+
+    /**
+     * Constructor.
+     *
+     * @param name name.
+     */
+    public Shop(String name) {
+        super(name);
+    }
+
+    /**
+     * Check condition.
+     *
+     * @param food food.
+     * @return true if the foods can be added.
+     */
+    @Override
+    public boolean validatesFood(final IFoods food) {
+        boolean flag = false;
+        double shelfLife = food.checkShelfLife();
+        if (shelfLife < maxLife && shelfLife > minLife) {
+            if (shelfLife <= discountLife) {
+                food.setDiscount(25);
+            }
+            flag = true;
+        }
+        return flag;
+    }
+}
