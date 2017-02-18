a88aab3dc134186600e5912a8fa5664f3c2fabc4
aleksey <fearerror@yandex.ru>
2017-02-18 23:38:33 +0400

    Interface IFoods  created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/food/IFoods.java
@@ -0,0 +1,58 @@
+package ru.job4j.food;
+
+import java.time.LocalDate;
+
+/**
+ * Interface foods.
+ */
+public interface IFoods {
+
+    /**
+     * Get.
+     *
+     * @return name.
+     */
+    String getName();
+
+    /**
+     * Get.
+     *
+     * @return create date.
+     */
+    LocalDate getCreateDate();
+
+    /**
+     * Get.
+     *
+     * @return expire date.
+     */
+    LocalDate getExpireDate();
+
+    /**
+     * Get.
+     *
+     * @return price.
+     */
+    double getPrice();
+
+    /**
+     * Get.
+     *
+     * @return discount.
+     */
+    int getDiscount();
+
+    /**
+     * Set discount.
+     *
+     * @param discount discount.
+     */
+    void setDiscount(int discount);
+
+    /**
+     * Method calculates the remaining shelf life.
+     *
+     * @return shelf life.
+     */
+    double checkShelfLife();
+}
