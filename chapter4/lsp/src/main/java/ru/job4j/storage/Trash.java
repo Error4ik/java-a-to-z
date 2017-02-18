20d3d143ed3797cad113bb94c71a2dbb8c3991d5
aleksey <fearerror@yandex.ru>
2017-02-18 23:41:18 +0400

    Trash class created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/storage/Trash.java
@@ -0,0 +1,38 @@
+package ru.job4j.storage;
+
+import ru.job4j.food.IFoods;
+
+/**
+ *
+ */
+public class Trash extends Storage {
+
+    /**
+     * Condition in add food.
+     */
+    private final double shelfLife = 0d;
+
+    /**
+     * Constructor.
+     *
+     * @param name name.
+     */
+    public Trash(String name) {
+        super(name);
+    }
+
+    /**
+     * Check condition.
+     *
+     * @param food food.
+     * @return true if the foods can be added.
+     */
+    @Override
+    public boolean validatesFood(final IFoods food) {
+        boolean flag = false;
+        if (food.checkShelfLife() == this.shelfLife) {
+            flag = true;
+        }
+        return flag;
+    }
+}
