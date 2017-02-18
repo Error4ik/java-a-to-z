4a25168613689cb3bfaa2835fd32b37d42981c7b
aleksey <fearerror@yandex.ru>
2017-02-18 23:39:10 +0400

    Milk class created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/food/Milk.java
@@ -0,0 +1,21 @@
+package ru.job4j.food;
+
+import java.time.LocalDate;
+
+/**
+ *
+ */
+public class Milk extends Foods {
+
+    /**
+     * Constructor.
+     *
+     * @param name       name.
+     * @param createDate create date..
+     * @param expireDate expire date.
+     * @param price      price.
+     */
+    public Milk(String name, LocalDate createDate, LocalDate expireDate, double price) {
+        super(name, createDate, expireDate, price);
+    }
+}
