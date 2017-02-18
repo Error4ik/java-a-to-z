a2808359baf268045ccfa16ac2c3a8f20f9b26c7
aleksey <fearerror@yandex.ru>
2017-02-18 23:40:27 +0400

    Interface IStorage created.

---
+++ chapter4/lsp/src/main/java/ru/job4j/storage/IStorage.java
@@ -0,0 +1,53 @@
+package ru.job4j.storage;
+
+import ru.job4j.food.IFoods;
+
+import java.util.List;
+
+/**
+ * Interface.
+ */
+public interface IStorage {
+
+    /**
+     * Method adds food in storage.
+     * @param food food.
+     * @return if added return true.
+     */
+    boolean addFood(final IFoods food);
+
+    /**
+     * Method remove food from storage.
+     *
+     * @param food food.
+     */
+    void removeFood(final IFoods food);
+
+    /**
+     * Method returns list food from storage.
+     *
+     * @return list.
+     */
+    List<IFoods> getFoodList();
+
+    /**
+     * Get.
+     *
+     * @return amountOfFoods.
+     */
+    int getAmountOfFoods();
+
+    /**
+     * Check condition.
+     *
+     * @param food food.
+     * @return true if the foods can be added.
+     */
+    boolean validatesFood(final IFoods food);
+
+    /**
+     * Get.
+     * @return name storage.
+     */
+    String getName();
+}
