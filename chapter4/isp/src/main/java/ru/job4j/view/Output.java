aeaa83bd618f8fc67389261010cc786663346ae8
aleksey <fearerror@yandex.ru>
2017-02-22 18:42:08 +0400

    Output interface created.

---
+++ chapter4/isp/src/main/java/ru/job4j/view/Output.java
@@ -0,0 +1,13 @@
+package ru.job4j.view;
+
+/**
+ * Output interface.
+ */
+public interface Output {
+
+    /**
+     * Print.
+     * @param string string.
+     */
+    void print(final String string);
+}
