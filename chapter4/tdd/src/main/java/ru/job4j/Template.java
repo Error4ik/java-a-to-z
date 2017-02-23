a82eef3f806bdb287d3e4eca0f2824feb70fe71d
aleksey <fearerror@yandex.ru>
2017-02-23 21:45:28 +0400

    Interface Template created.

---
+++ chapter4/tdd/src/main/java/ru/job4j/Template.java
@@ -0,0 +1,42 @@
+package ru.job4j;
+
+import java.util.Map;
+
+/**
+ * @author Alexey Voronin.
+ * @since 23.02.2017.
+ */
+public interface Template {
+
+    /**
+     * Template Hello ${name}.
+     *
+     * @param template template.
+     * @return string.
+     */
+    String generate(final String template);
+
+    /**
+     * Add Template to the map.
+     *
+     * @param key   the key according to which you want to add the value.
+     * @param value value that is added by a key.
+     * @return true if value to added.
+     */
+    boolean addTemplateToTheMap(final String key, final String value);
+
+    /**
+     * Remove template from map.
+     *
+     * @param key the key according to which you want to delete the value.
+     * @return true if value to removed.
+     */
+    boolean removeTemplateFromMap(final String key);
+
+    /**
+     * Get.
+     *
+     * @return map.
+     */
+    Map<String, String> getMap();
+}
