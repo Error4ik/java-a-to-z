f07687e8c29a9568947958a104204fc1ae83923f
aleksey <fearerror@yandex.ru>
2017-02-27 21:48:48 +0400

    Figure class created.

---
+++ chapter4/test-task/src/main/java/ru/job4j/model/Figure.java
@@ -0,0 +1,15 @@
+package ru.job4j.model;
+
+/**
+ * Figures to game.
+ *
+ * @author Alexey Voronin.
+ * @since 24.02.2017.
+ */
+public enum Figure {
+
+    /**
+     * Two figure.
+     */
+    X, O
+}
