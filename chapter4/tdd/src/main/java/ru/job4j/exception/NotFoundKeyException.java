f72ab5cd8bb6ab5b129a8f83a452ca4a62ce09d3
aleksey <fearerror@yandex.ru>
2017-02-28 15:02:54 +0400

    NotFoundKeyException class created.

---
+++ chapter4/tdd/src/main/java/ru/job4j/exception/NotFoundKeyException.java
@@ -0,0 +1,13 @@
+package ru.job4j.exception;
+
+/**
+ * TODO: comment.
+ *
+ * @author Alexey Voronin.
+ * @since 28.02.2017.
+ */
+public class NotFoundKeyException extends Exception{
+    public NotFoundKeyException(final String message) {
+        super(message);
+    }
+}
