48e9f1127906bf10ba78ec14b7aab4d8d9dd2b87
aleksey <fearerror@yandex.ru>
2017-03-07 15:32:48 +0400

    ArrayIterator class created.

---
+++ chapter5/iterator/src/main/java/ru/job4j/ArrayIterator.java
@@ -0,0 +1,49 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Array iterator.
+ *
+ * @author Alexey Voronin.
+ * @since 07.03.2017.
+ */
+public class ArrayIterator implements Iterator<Integer> {
+
+    /**
+     * Array int value.
+     */
+    private final int[] array;
+
+    /**
+     * Index array.
+     */
+    private int index = 0;
+
+    /**
+     * Constructor.
+     *
+     * @param array input array.
+     */
+    public ArrayIterator(final int[] array) {
+        this.array = array;
+    }
+
+    /**
+     * @return true if has more element.
+     */
+    @Override
+    public boolean hasNext() {
+        return this.array.length > index;
+    }
+
+    @Override
+    public Integer next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException("No more items.");
+        }
+        return this.array[index++];
+    }
+
+}
