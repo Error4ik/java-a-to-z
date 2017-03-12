6015e84df00d2be050d474d53b4bbe63e1730351
aleksey <fearerror@yandex.ru>
2017-03-12 20:49:23 +0400

    UserStore class created.

---
+++ chapter5/generic/src/main/java/ru/job4j/store/UserStore.java
@@ -0,0 +1,18 @@
+package ru.job4j.store;
+
+/**
+ * User IStore.
+ *
+ * @author Alexey Voronin.
+ * @since 12.03.2017.
+ */
+public class UserStore extends BaseStore {
+
+    /**
+     * Constructor.
+     * @param size size to storage.
+     */
+    public UserStore(int size) {
+        super(size);
+    }
+}
