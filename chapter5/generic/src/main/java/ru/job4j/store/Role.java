0168887ce3efa9062e35825a50589c409e0d3442
aleksey <fearerror@yandex.ru>
2017-03-12 20:48:42 +0400

    Role class created.

---
+++ chapter5/generic/src/main/java/ru/job4j/store/Role.java
@@ -0,0 +1,18 @@
+package ru.job4j.store;
+
+/**
+ * Role.
+ *
+ * @author Alexey Voronin.
+ * @since 12.03.2017.
+ */
+public class Role extends Base {
+
+    /**
+     * Constructor.
+     * @param id id to the object.
+     */
+    public Role(final String id) {
+        super(id);
+    }
+}
