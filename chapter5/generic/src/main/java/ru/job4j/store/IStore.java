42a587220386ed00fb5a07d29655ed52b4a48b84
aleksey <fearerror@yandex.ru>
2017-03-12 20:47:54 +0400

    Interface IStore created.

---
+++ chapter5/generic/src/main/java/ru/job4j/store/IStore.java
@@ -0,0 +1,33 @@
+package ru.job4j.store;
+
+/**
+ * Interface IStore.
+ *
+ * @param <T> any type to extended Base class.
+ * @author Alexey Voronin.
+ * @since 12.03.2017.
+ */
+public interface IStore<T extends Base> {
+
+    /**
+     * Add value to array.
+     *
+     * @param value value.
+     */
+    void add(T value);
+
+    /**
+     * Delete value from array.
+     *
+     * @param value value to need deleted.
+     */
+    void delete(T value);
+
+    /**
+     * Replace value in array.
+     *
+     * @param value    value to need replaced.
+     * @param newValue new value.
+     */
+    void update(T value, T newValue);
+}
