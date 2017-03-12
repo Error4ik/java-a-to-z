817ea3d92c1deb1c053cc1254f27e29ee2ae6470
aleksey <fearerror@yandex.ru>
2017-03-12 20:48:56 +0400

    RoleStore class created.

---
+++ chapter5/generic/src/main/java/ru/job4j/store/RoleStore.java
@@ -0,0 +1,19 @@
+package ru.job4j.store;
+
+/**
+ * RoleStore.
+ *
+ * @author Alexey Voronin.
+ * @since 12.03.2017.
+ */
+public class RoleStore extends BaseStore {
+
+    /**
+     * Constructor.
+     *
+     * @param size size to storage.
+     */
+    public RoleStore(int size) {
+        super(size);
+    }
+}
