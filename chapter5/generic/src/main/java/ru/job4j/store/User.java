e8496885bfc64ecf540a84b3d70aed769f035f18
aleksey <fearerror@yandex.ru>
2017-03-12 20:49:09 +0400

    User class created.

---
+++ chapter5/generic/src/main/java/ru/job4j/store/User.java
@@ -0,0 +1,18 @@
+package ru.job4j.store;
+
+/**
+ * User.
+ *
+ * @author Alexey Voronin.
+ * @since 12.03.2017.
+ */
+public class User extends Base {
+
+    /**
+     * Constructor.
+     * @param id id to the object.
+     */
+    public User(final String id) {
+        super(id);
+    }
+}
