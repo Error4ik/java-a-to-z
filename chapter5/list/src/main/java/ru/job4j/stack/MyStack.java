f6e744bbe5f6f84f195dac19b9788d4976cc4826
aleksey <fearerror@yandex.ru>
2017-03-14 20:34:08 +0400

    MyStack class created.

---
+++ chapter5/list/src/main/java/ru/job4j/stack/MyStack.java
@@ -0,0 +1,44 @@
+package ru.job4j.stack;
+
+import ru.job4j.linked_list.MyLinkedList;
+
+/**
+ * My Stack.
+ *
+ * @param <T>
+ * @author Alexey Voronin.
+ * @since 14.03.2017.
+ */
+public class MyStack<T> {
+
+    /**
+     * Container by contains items to my stack.
+     */
+    private final MyLinkedList<T> list;
+
+    /**
+     * Constructor.
+     * Init the container.
+     */
+    public MyStack() {
+        this.list = new MyLinkedList<T>();
+    }
+
+    /**
+     * Add item to my stack.
+     *
+     * @param t item to added.
+     */
+    public void push(final T t) {
+        this.list.add(t);
+    }
+
+    /**
+     * Removes the object at the top of this stack and returns that object as the value of this function.
+     *
+     * @return item.
+     */
+    public T pop() {
+        return this.list.get(this.list.getSize() - 1);
+    }
+}
