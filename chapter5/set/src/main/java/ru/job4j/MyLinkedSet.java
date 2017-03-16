297d789ba592b35fc7658f6ee79619eca74d433f
aleksey <fearerror@yandex.ru>
2017-03-16 15:27:32 +0400

    MyLinkedSet created.

---
+++ chapter5/set/src/main/java/ru/job4j/MyLinkedSet.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+import ru.job4j.linked_list.MyLinkedList;
+
+import java.util.Iterator;
+
+/**
+ * My Set to linkedList.
+ *
+ * @param <T> any type.
+ * @author Alexey Voronin.
+ * @since 15.03.2017.
+ */
+public class MyLinkedSet<T> extends MyLinkedList<T> {
+
+    @Override
+    public void add(T t) {
+        if (t != null) {
+            boolean flag = true;
+            for (T value : this) {
+                if (t.equals(value)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                super.add(t);
+            }
+        }
+    }
+}
