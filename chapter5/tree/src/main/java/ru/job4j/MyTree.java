306d972eb922987a144c5dd7da6a09a01129b6b1
aleksey <fearerror@yandex.ru>
2017-03-24 14:03:36 +0400

    MyTree class created.

--- chapter5/tree/src/main/java/ru/job4j/MyTree.java
+++ chapter5/tree/src/main/java/ru/job4j/MyTree.java
@@ -2,125 +2,49 @@ package ru.job4j;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * My Node.
+ * My MyTree.
  *
  * @param <T> any type.
  * @author Alexey Voronin.
  * @since 22.03.2017.
  */
-public class Node<T> {
+public class MyTree<T> {
 
     /**
-     * Value this node.
+     * Root node.
      */
-    private T value;
+    private Node<T> root;
 
     /**
-     * First child.
-     */
-    private Node<T> parent;
-
-    /**
-     * List of descendants.
-     */
-    private List<Node<T>> childList;
-
-    /**
-     * Constructor.
+     * Add child.
      *
-     * @param value any type value.
-     */
-    public Node(final T value) {
-        this.value = value;
-        this.childList = new LinkedList<>();
-    }
-
-    /**
-     * Add child to parent.
-     *
-     * @param parent parent.
+     * @param parent root node.
      * @param child  child.
      */
     public void addChild(final Node<T> parent, final Node<T> child) {
-        parent.addChild(child);
-        child.parent = parent;
-    }
-
-    /**
-     * Method add child to list.
-     *
-     * @param newChild child.
-     */
-    public void addChild(final Node<T> newChild) {
-        this.childList.add(newChild);
-    }
-
-    /**
-     * If there is no parent, then this is the root.
-     *
-     * @return true if root.
-     */
-    public boolean isRoot() {
-        return this.parent == null;
-    }
-
-    /**
-     * Get.
-     *
-     * @return the list of children.
-     */
-    public List<Node<T>> getChildList() {
-        return this.childList;
-    }
-
-    /**
-     * Check if the node is in the tree.
-     *
-     * @param value find value.
-     * @return true if tree contains to node.
-     */
-    public boolean contains(final T value) {
-        Node<T> currentNode = this;
-        boolean flag = false;
-        if (this.getValue().equals(value)) {
-            flag = true;
+        if (root == null) {
+            root = parent;
         }
-        if (!flag && )
-
-
-
-
-        boolean added = false;
-        if (currentLeaf.getRoot().equals(e)) {
-            added = true;
-        }
-        if (!added && currentLeaf.getLeft() != null) {
-            added = this.findInLeaf(e, currentLeaf.getLeft());
-        }
-        if (!added && currentLeaf.getRight() != null) {
-            added = this.findInLeaf(e, currentLeaf.getRight());
-        }
-        return added;
-        return flag;
+        parent.getChild().add(child);
     }
 
-    /**
-     * Get.
-     *
-     * @return value.
-     */
-    public T getValue() {
-        return value;
-    }
 
     /**
-     * If you do not have children, and this is not the root node, then this is a leaf.
+     * Method return all child value to root.
      *
-     * @return true if leaf.
+     * @param root root node.
+     * @return list value.
      */
-    public boolean isLeaf() {
-        return this.childList.size() == 0 && !this.isRoot();
+    public List<T> getChild(final Node<T> root) {
+        List<T> child = new LinkedList<T>();
+        if (root.getChild().size() != 0) {
+            for (Node<T> node : root.getChild()) {
+                child.add(node.getValue());
+            }
+        }
+        return child;
     }
 }
