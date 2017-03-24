59effc26cd8bce1d32db03914b4267e229e60111
aleksey <fearerror@yandex.ru>
2017-03-24 14:03:24 +0400

    Node class created.

---
+++ chapter5/tree/src/main/java/ru/job4j/Node.java
@@ -0,0 +1,51 @@
+package ru.job4j;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Node to tree.
+ *
+ * @author Alexey Voronin.
+ * @since 24.03.2017.
+ */
+public class Node<T> {
+
+    /**
+     * Value.
+     */
+    private T value;
+
+    /**
+     * List child.
+     */
+    private List<Node<T>> child;
+
+    /**
+     * Constructor.
+     *
+     * @param value value.
+     */
+    public Node(final T value) {
+        this.value = value;
+        this.child = new LinkedList<>();
+    }
+
+    /**
+     * Get.
+     *
+     * @return value.
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * Get.
+     *
+     * @return list child.
+     */
+    public List<Node<T>> getChild() {
+        return child;
+    }
+}
