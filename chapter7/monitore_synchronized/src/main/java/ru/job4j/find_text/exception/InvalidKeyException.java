f89c054cf85be849bf6f77fa18a58232469c10a8
aleksey <fearerror@yandex.ru>
2017-04-30 09:15:58 +0400

    InvalidKeyException class created #5.

---
+++ chapter7/monitore_synchronized/src/main/java/ru/job4j/find_text/exception/InvalidKeyException.java
@@ -0,0 +1,16 @@
+package ru.job4j.find_text.exception;
+
+/**
+ * Exception invalid key.
+ */
+public class InvalidKeyException extends Exception {
+
+    /**
+     * Constructor.
+     *
+     * @param message message.
+     */
+    public InvalidKeyException(final String message) {
+        super(message);
+    }
+}
