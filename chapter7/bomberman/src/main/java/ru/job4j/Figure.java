f17bf13d13707a1c9b2833ad339a0fca851328c5
aleksey <fearerror@yandex.ru>
2017-05-10 02:12:40 +0400

    Figure class created.  #10.

---
+++ chapter7/bomberman/src/main/java/ru/job4j/Figure.java
@@ -0,0 +1,61 @@
+package ru.job4j;
+
+/**
+ * Figure.
+ *
+ * @author Alexey Voronin.
+ * @since 09.05.2017.
+ */
+public abstract class Figure {
+
+    /**
+     * Field.
+     */
+    private final Field field;
+
+    /**
+     * Position figure.
+     */
+    private Point point;
+
+    /**
+     * Constructor.
+     *
+     * @param field field.
+     */
+    public Figure(final Field field) {
+        this.field = field;
+    }
+
+    /**
+     * Set.
+     *
+     * @param point new Point.
+     */
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    /**
+     * Get.
+     *
+     * @return point.
+     */
+    public Point getPoint() {
+        return point;
+    }
+
+    /**
+     * Get.
+     *
+     * @return field.
+     */
+    public Field getField() {
+        return field;
+    }
+
+    /**
+     * Motions of the figure by field.
+     */
+    abstract void moveFigure();
+}
