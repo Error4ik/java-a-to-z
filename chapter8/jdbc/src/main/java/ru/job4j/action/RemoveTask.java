d2f4b8666eae6a173244bb75e252f13d092f3747
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:07 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/RemoveTask.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/RemoveTask.java
@@ -1,10 +1,48 @@
 package ru.job4j.action;
 
+import org.apache.log4j.Logger;
+import ru.job4j.input.Input;
+import ru.job4j.tracker.Tracker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
- * TODO: comment.
+ * Query Remove task.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class RemoveTask {
+public class RemoveTask extends BaseAction {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(RemoveTask.class);
+
+    /**
+     * Constructor.
+     *
+     * @param id   action id.
+     * @param name action name.
+     */
+    public RemoveTask(final String id, final String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void execute(final Tracker tracker, final Input inputData) {
+        final int id = Integer.parseInt(inputData.getInput("Enter the ID for the task you want to delete: ").trim());
+        try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
+             PreparedStatement ps = con.prepareStatement("BEGIN; DELETE FROM comments WHERE task_id = ?;"
+                     + " DELETE FROM task WHERE id = ?; COMMIT")) {
+            ps.setInt(1, id);
+            ps.setInt(2, id);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
 }
