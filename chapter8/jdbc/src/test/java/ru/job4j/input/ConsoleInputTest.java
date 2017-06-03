3a2b9cb191acf0b817e92a248351bc345d0b3741
aleksey <fearerror@yandex.ru>
2017-06-03 20:35:03 +0400

    add #15.

--- chapter8/jdbc/src/test/java/ru/job4j/input/ConsoleInputTest.java
+++ chapter8/jdbc/src/test/java/ru/job4j/input/ConsoleInputTest.java
@@ -1,10 +1,33 @@
 package ru.job4j.input;
 
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
 /**
- * TODO: comment.
+ * Console view test.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
 public class ConsoleInputTest {
+
+    /**
+     * getInput.
+     */
+    @Test
+    public void whenGetInputThenReturnMessageAndUserInput() {
+        final ConsoleInput inputData = new ConsoleInput();
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream("Hello".getBytes());
+        System.setIn(inputStream);
+        final String expectedValue = "Hello";
+
+        String actualValue = inputData.getInput("Say Hello");
+
+        assertThat(actualValue, is(expectedValue));
+    }
 }
