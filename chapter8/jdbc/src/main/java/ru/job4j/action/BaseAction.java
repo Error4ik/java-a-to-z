5726755185cc8216314b498e418d7bae0e36bbbd
aleksey <fearerror@yandex.ru>
2017-06-03 20:31:29 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/BaseAction.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/BaseAction.java
@@ -1,10 +1,60 @@
 package ru.job4j.action;
 
 /**
- * TODO: comment.
+ * Base class to all action..
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class BaseAction {
+public abstract class BaseAction implements Action {
+
+    /**
+     * Action name.
+     */
+    private final String name;
+
+    /**
+     * Action id.
+     */
+    private final String id;
+
+    /**
+     * Constructor.
+     *
+     * @param id   action id.
+     * @param name action name.
+     */
+    public BaseAction(final String id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Метод для отображения порядкового номера и названия действия.
+     *
+     * @return action number and name.
+     */
+    @Override
+    public String showItem() {
+        return String.format("%s. %s", this.getId(), this.getName());
+    }
+
+    /**
+     * Get.
+     *
+     * @return action name.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Get.
+     *
+     * @return action id.
+     */
+    @Override
+    public String getId() {
+        return this.id;
+    }
 }
