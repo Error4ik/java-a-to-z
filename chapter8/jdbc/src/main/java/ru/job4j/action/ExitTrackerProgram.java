de69bd2f379c03440b8b1f59b4cd00fb54c2f448
aleksey <fearerror@yandex.ru>
2017-06-03 20:31:38 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/ExitTrackerProgram.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/ExitTrackerProgram.java
@@ -1,10 +1,29 @@
 package ru.job4j.action;
 
+import ru.job4j.input.Input;
+import ru.job4j.tracker.Tracker;
+import ru.job4j.view.ConsoleView;
+
 /**
- * TODO: comment.
+ * Exit from program.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class ExitTrackerProgram {
+public class ExitTrackerProgram extends BaseAction {
+
+    /**
+     * Constructor.
+     *
+     * @param id   action id.
+     * @param name action name.
+     */
+    public ExitTrackerProgram(final String id, final String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void execute(final Tracker tracker, final Input inputData) {
+        new ConsoleView().execute(String.format("Exit program%s", System.getProperty("line.separator")));
+    }
 }
