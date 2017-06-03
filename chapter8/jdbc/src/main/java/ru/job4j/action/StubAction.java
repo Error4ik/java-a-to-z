96ca436b88a3b346a77f6095b16ea6ed6fa66e18
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:22 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/StubAction.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/StubAction.java
@@ -1,10 +1,29 @@
 package ru.job4j.action;
 
+import ru.job4j.input.Input;
+import ru.job4j.tracker.Tracker;
+import ru.job4j.view.ConsoleView;
+
 /**
- * TODO: comment.
+ * Stub action.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class StubAction {
+public class StubAction extends BaseAction {
+
+    /**
+     * Constructor.
+     *
+     * @param id   action id.
+     * @param name action name.
+     */
+    public StubAction(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void execute(Tracker tracker, Input inputData) {
+        new ConsoleView().execute(String.format("WORK!%s", System.getProperty("line.separator")));
+    }
 }
