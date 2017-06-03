50e02ce42933bc71cd7fc95da7399fc0b0b66cb4
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:51 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/input/Input.java
+++ chapter8/jdbc/src/main/java/ru/job4j/input/Input.java
@@ -1,10 +1,19 @@
 package ru.job4j.input;
 
 /**
- * TODO: comment.
+ * User input.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
 public interface Input {
+
+    /**
+     * Returns user input.
+     *
+     * @param msg message to user.
+     * @return user input.
+     * @throws NumberFormatException If, when selecting a menu item, not a number, but a string.
+     */
+    String getInput(String msg);
 }
