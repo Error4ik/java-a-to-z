8305ea397a150253c8916c4a0dbac20ae9ab1e36
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:46 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/input/ConsoleInput.java
+++ chapter8/jdbc/src/main/java/ru/job4j/input/ConsoleInput.java
@@ -1,10 +1,19 @@
 package ru.job4j.input;
 
+import java.util.Scanner;
+
 /**
- * TODO: comment.
+ * User's console input.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class ConsoleInput {
+public class ConsoleInput implements Input {
+
+    @Override
+    public String getInput(final String msg) throws NumberFormatException {
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        System.out.print(msg);
+        return scanner.nextLine();
+    }
 }
