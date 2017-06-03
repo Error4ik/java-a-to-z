f8c991b5cb0c94d35db8861e54e15e596c7bb7c4
aleksey <fearerror@yandex.ru>
2017-06-03 20:31:59 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/RemoveComment.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/RemoveComment.java
@@ -1,10 +1,47 @@
 package ru.job4j.action;
 
+import org.apache.log4j.Logger;
+import ru.job4j.input.Input;
+import ru.job4j.tracker.Tracker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
- * TODO: comment.
+ * Query Remove comment to task.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class RemoveComment {
+public class RemoveComment extends BaseAction {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(RemoveComment.class);
+
+    /**
+     * Constructor.
+     *
+     * @param id   action id.
+     * @param name action name.
+     */
+    public RemoveComment(final String id, final String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void execute(final Tracker tracker, final Input inputData) {
+        int id = Integer.parseInt(
+                inputData.getInput("Enter the ID you want to delete a comment: ").trim());
+        try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
+             PreparedStatement ps = con.prepareStatement("DELETE FROM comments WHERE id = ?")) {
+            ps.setInt(1, id);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
 }
