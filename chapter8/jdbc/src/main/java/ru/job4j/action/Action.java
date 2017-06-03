f3a8abe04119b47f37e9d2479db9eb06f3a0536a
aleksey <fearerror@yandex.ru>
2017-06-03 20:31:08 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/Action.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/Action.java
@@ -1,10 +1,35 @@
 package ru.job4j.action;
 
+import ru.job4j.input.Input;
+import ru.job4j.tracker.Tracker;
+
 /**
- * TODO: comment.
+ * Interface for actions.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
 public interface Action {
+
+    /**
+     * Returns the number under which this action is in the menu.
+     *
+     * @return number.
+     */
+    String getId();
+
+    /**
+     * Displaying information about the action.
+     *
+     * @return string with the number and name of the action.
+     */
+    String showItem();
+
+    /**
+     * Performs all the main work of the menu item.
+     *
+     * @param tracker   tracker.
+     * @param inputData user input.
+     */
+    void execute(final Tracker tracker, final Input inputData);
 }
