67abc08cc2d41471e6aee685cd63ff3ef1695cd9
aleksey <fearerror@yandex.ru>
2017-06-03 20:35:25 +0400

    add #15.

--- chapter8/jdbc/src/test/java/ru/job4j/models/TaskTest.java
+++ chapter8/jdbc/src/test/java/ru/job4j/models/TaskTest.java
@@ -1,10 +1,51 @@
 package ru.job4j.models;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * TODO: comment.
+ * Task test.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
 public class TaskTest {
+
+    /**
+     * GetName().
+     */
+    @Test
+    public void whenGetNameThenReturnValidName() {
+        final Task task = new Task(0, "Task", null, 0);
+        final String expectedValue = "Task";
+
+        final String actualValue = task.getName();
+        assertThat(actualValue, is(expectedValue));
+    }
+
+    /**
+     * GetDescription().
+     */
+    @Test
+    public void whenGetDescriptionThenReturnValidDescription() {
+        final Task task = new Task(0, null, "Desc", 0);
+        final String expectedValue = "Desc";
+
+        final String actualValue = task.getDescription();
+        assertThat(actualValue, is(expectedValue));
+    }
+
+    /**
+     * GetId().
+     */
+    @Test
+    public void whenGetIdThenReturnValidId() {
+        final Task task = new Task(5, null, null, 0);
+        final int expectedValue = 5;
+
+        final int actualValue = task.getId();
+        assertThat(actualValue, is(expectedValue));
+    }
 }
