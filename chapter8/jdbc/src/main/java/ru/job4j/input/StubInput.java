b90bab4a991a12799009161568ad8d43a1767dd1
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:59 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/input/StubInput.java
+++ chapter8/jdbc/src/main/java/ru/job4j/input/StubInput.java
@@ -1,10 +1,34 @@
 package ru.job4j.input;
 
 /**
- * TODO: comment.
+ * StubInput that simulates user input.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class StubInput {
+public class StubInput implements Input {
+
+    /**
+     * An array that simulates user input.
+     */
+    private final String[] answers;
+
+    /**
+     * Position to array.
+     */
+    private int position = 0;
+
+    /**
+     * Constructor.
+     *
+     * @param array input user array.
+     */
+    public StubInput(final String[] array) {
+        this.answers = array;
+    }
+
+    @Override
+    public String getInput(String msg) {
+        return answers[position++];
+    }
 }
