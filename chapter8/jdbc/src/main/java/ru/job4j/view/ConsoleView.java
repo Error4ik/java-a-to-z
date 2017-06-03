186891e1be652231c9319b79e6aed9ec065edeb5
aleksey <fearerror@yandex.ru>
2017-06-03 20:34:08 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/view/ConsoleView.java
+++ chapter8/jdbc/src/main/java/ru/job4j/view/ConsoleView.java
@@ -1,10 +1,17 @@
 package ru.job4j.view;
 
 /**
- * TODO: comment.
+ * Print to console.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class ConsoleView {
+public class ConsoleView implements IView {
+
+    @Override
+    public void execute(final String string) {
+        System.out.println("===========================================================");
+        System.out.print(string);
+        System.out.println("===========================================================");
+    }
 }
