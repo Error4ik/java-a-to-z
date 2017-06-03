efc06daeb700dcb85ed19e5db4ec10d56781e21a
aleksey <fearerror@yandex.ru>
2017-06-03 20:32:31 +0400

    add #15.

--- chapter8/jdbc/src/main/java/ru/job4j/exception/MenuOutException.java
+++ chapter8/jdbc/src/main/java/ru/job4j/exception/MenuOutException.java
@@ -1,10 +1,19 @@
 package ru.job4j.exception;
 
 /**
- * TODO: comment.
+ * Throws when you go beyond the menu items.
  *
  * @author Alexey Voronin.
  * @since 03.06.2017.
  */
-public class MenuOutException {
+public class MenuOutException extends RuntimeException {
+
+    /**
+     * Constructor.
+     *
+     * @param msg message.
+     */
+    public MenuOutException(final String msg) {
+        System.out.print(msg);
+    }
 }
