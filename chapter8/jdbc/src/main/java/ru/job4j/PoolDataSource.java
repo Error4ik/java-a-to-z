5ea4b6202b8567a1b816152f0feef5230041fdb3
aleksey <fearerror@yandex.ru>
2017-06-12 23:55:05 +0400

    PoolDataSource class created #15.

---
+++ chapter8/jdbc/src/main/java/ru/job4j/PoolDataSource.java
@@ -0,0 +1,40 @@
+package ru.job4j;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
+
+/**
+ * Pool connections.
+ *
+ * @author Alexey Voronin.
+ * @since 08.06.2017.
+ */
+public class PoolDataSource {
+
+    /**
+     * Create a ConnectionFactory that the pool will use to create Connections.
+     * Create the PoolableConnectionFactory, which wraps the "real" Connections.
+     * Set the factory's pool property to the owning pool.
+     * Now we'll need a ObjectPool that serves as the actual pool of connections.
+     * Create the PoolingDriver itself, passing in the object pool we created.
+     *
+     * @param url      url.
+     * @param userName user name.
+     * @param password password.
+     * @return dataSource.
+     */
+    public static DataSource setupDataSource(final String url, final String userName, final String password) {
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(url, userName, password);
+        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, null);
+        ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnectionFactory);
+        poolableConnectionFactory.setPool(connectionPool);
+        return new PoolingDataSource<>(connectionPool);
+    }
+}
