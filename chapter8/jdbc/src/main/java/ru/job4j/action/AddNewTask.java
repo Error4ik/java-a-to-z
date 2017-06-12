85fe78dbcc62f488682dc68c20c772439b660363
aleksey <fearerror@yandex.ru>
2017-06-12 23:45:27 +0400

    change execute method #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/AddNewTask.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/AddNewTask.java
@@ -1,14 +1,8 @@
 package ru.job4j.action;
 
-import org.apache.log4j.Logger;
+import ru.job4j.dao.ITracker;
 import ru.job4j.input.Input;
-import ru.job4j.tracker.Tracker;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import ru.job4j.models.Task;
 
 /**
  * Query Adds new task to DB.
@@ -17,11 +11,6 @@ import java.sql.Timestamp;
  * @since 03.06.2017.
  */
 public class AddNewTask extends BaseAction {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(AddNewTask.class);
 
     /**
      * Constructor.
@@ -34,19 +23,10 @@ public class AddNewTask extends BaseAction {
     }
 
     @Override
-    public void execute(final Tracker tracker, final Input inputData) {
-        String name = inputData.getInput("Enter the name new task: ").trim();
-        String description = inputData.getInput("Enter the description new task: ").trim();
-        if (!name.equals("")) {
-            try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
-                 PreparedStatement ps = con.prepareStatement("INSERT INTO task(name, description, create_date) VALUES (?, ?, ?)")) {
-                ps.setString(1, name);
-                ps.setString(2, description);
-                ps.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
-                ps.executeQuery();
-            } catch (SQLException e) {
-                LOGGER.error(e.getMessage(), e);
-            }
-        }
+    public void execute(final ITracker tracker, final Input inputData) {
+        final String name = inputData.getInput("Enter the name new task: ").trim();
+        final String description = inputData.getInput("Enter the description new task: ").trim();
+        final Task task = new Task(0, name, description, System.currentTimeMillis());
+        tracker.addTask(task);
     }
 }
