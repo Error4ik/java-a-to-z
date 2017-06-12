75ae76c977c07bca3bbadcdc50bfbe583e21773a
aleksey <fearerror@yandex.ru>
2017-06-12 23:45:51 +0400

    change execute method #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/RemoveComment.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/RemoveComment.java
@@ -1,13 +1,7 @@
 package ru.job4j.action;
 
-import org.apache.log4j.Logger;
+import ru.job4j.dao.ITracker;
 import ru.job4j.input.Input;
-import ru.job4j.tracker.Tracker;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 /**
  * Query Remove comment to task.
@@ -16,11 +10,6 @@ import java.sql.SQLException;
  * @since 03.06.2017.
  */
 public class RemoveComment extends BaseAction {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(RemoveComment.class);
 
     /**
      * Constructor.
@@ -33,15 +22,9 @@ public class RemoveComment extends BaseAction {
     }
 
     @Override
-    public void execute(final Tracker tracker, final Input inputData) {
-        int id = Integer.parseInt(
+    public void execute(final ITracker tracker, final Input inputData) {
+        final int commentID = Integer.parseInt(
                 inputData.getInput("Enter the ID you want to delete a comment: ").trim());
-        try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
-             PreparedStatement ps = con.prepareStatement("DELETE FROM comments WHERE id = ?")) {
-            ps.setInt(1, id);
-            ps.executeQuery();
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-        }
+        tracker.removeComment(commentID);
     }
 }
