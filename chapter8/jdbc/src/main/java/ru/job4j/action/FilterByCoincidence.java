7d61cdeae05e69c8a67b5b4705872b52df8878b1
aleksey <fearerror@yandex.ru>
2017-06-12 23:45:39 +0400

    change execute method #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/FilterByCoincidence.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/FilterByCoincidence.java
@@ -1,15 +1,11 @@
 package ru.job4j.action;
 
-import org.apache.log4j.Logger;
+import ru.job4j.dao.ITracker;
 import ru.job4j.input.Input;
-import ru.job4j.tracker.Tracker;
+import ru.job4j.models.Task;
 import ru.job4j.view.ConsoleView;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * Query Filter by coincidence.
@@ -18,11 +14,6 @@ import java.sql.ResultSet;
  * @since 03.06.2017.
  */
 public class FilterByCoincidence extends BaseAction {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(FilterByCoincidence.class);
 
     /**
      * Constructor.
@@ -35,21 +26,12 @@ public class FilterByCoincidence extends BaseAction {
     }
 
     @Override
-    public void execute(final Tracker tracker, final Input inputData) {
+    public void execute(final ITracker tracker, final Input inputData) {
         StringBuilder sb = new StringBuilder();
         final String filter = inputData.getInput("Enter filter: ").trim();
-        try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
-             PreparedStatement ps = con.prepareStatement("SELECT * FROM task WHERE name LIKE ?")) {
-            ps.setString(1, String.format("%s%s%s", "%", filter, "%"));
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    sb.append(String.format("%d, %s, %s %s", rs.getInt("id"), rs.getString("name"),
-                            rs.getString("description"), rs.getTimestamp("create_date")))
-                            .append(System.getProperty("line.separator"));
-                }
-            }
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
+        List<Task> tasks = tracker.getTaskByCoincidence(filter);
+        for (Task task : tasks) {
+            sb.append(task).append(System.getProperty("line.separator"));
         }
         new ConsoleView().execute(sb.toString());
     }
