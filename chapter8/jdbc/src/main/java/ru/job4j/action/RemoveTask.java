c6a415239e4cdf92abe17b89b020c226134abc88
aleksey <fearerror@yandex.ru>
2017-06-12 23:45:55 +0400

    change execute method #15.

--- chapter8/jdbc/src/main/java/ru/job4j/action/RemoveTask.java
+++ chapter8/jdbc/src/main/java/ru/job4j/action/RemoveTask.java
@@ -1,13 +1,7 @@
 package ru.job4j.action;
 
-import org.apache.log4j.Logger;
+import ru.job4j.dao.ITracker;
 import ru.job4j.input.Input;
-import ru.job4j.tracker.Tracker;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 /**
  * Query Remove task.
@@ -16,11 +10,6 @@ import java.sql.SQLException;
  * @since 03.06.2017.
  */
 public class RemoveTask extends BaseAction {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(RemoveTask.class);
 
     /**
      * Constructor.
@@ -33,16 +22,8 @@ public class RemoveTask extends BaseAction {
     }
 
     @Override
-    public void execute(final Tracker tracker, final Input inputData) {
-        final int id = Integer.parseInt(inputData.getInput("Enter the ID for the task you want to delete: ").trim());
-        try (Connection con = DriverManager.getConnection(tracker.getUrl(), tracker.getUserName(), tracker.getPassword());
-             PreparedStatement ps = con.prepareStatement("BEGIN; DELETE FROM comments WHERE task_id = ?;"
-                     + " DELETE FROM task WHERE id = ?; COMMIT")) {
-            ps.setInt(1, id);
-            ps.setInt(2, id);
-            ps.executeQuery();
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-        }
+    public void execute(final ITracker tracker, final Input inputData) {
+        final int taskID = Integer.parseInt(inputData.getInput("Enter the ID for the task you want to delete: ").trim());
+        tracker.removeTaskById(taskID);
     }
 }
