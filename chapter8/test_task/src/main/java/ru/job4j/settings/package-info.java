ed283247808d003f92849e7a7e5ea614de128627
aleksey <fearerror@yandex.ru>
2017-07-03 15:02:31 +0400

    package-info.

---
+++ chapter8/test_task/src/main/java/ru/job4j/settings/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Alexey Voronin.
+ * @since 03.06.2017.
+ */
+package ru.job4j.settings;
---
+++ chapter8/test_task/src/test/java/ru/job4j/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Alexey Voronin.
+ * @since 03.07.2017.
+ */
+package ru.job4j.model;
